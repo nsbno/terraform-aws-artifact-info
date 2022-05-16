4aa4e7925fcd01bcf51651ff5d68797c81949a60
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-05-16 14:06:20 +0200

    Fix that output from module did not give vaules
    SSM Parameter objects are a bit TMI.

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "version" {
   description = "The artifacts version."
-  value       = data.aws_ssm_parameter.version
+  value       = data.aws_ssm_parameter.version.value
 }
 
 output "location" {
   description = "The location of the artifact. Typically the S3 Bucket name or the ECR Registry URI."
-  value       = data.aws_ssm_parameter.version
+  value       = data.aws_ssm_parameter.version.value
 }
 
 output "path" {
   description = "Where the artifact is stored. Typically an S3 prefix or an ECR Repository name."
-  value       = data.aws_ssm_parameter.version
+  value       = data.aws_ssm_parameter.version.value
 }
