9f3460958ebdd2dd485448eed5e34b6f5e802f2f
Nicolas Harlem Eide <nicolas@harlemeide.net>
2022-05-16 15:56:25 +0200

    Fix that `location` and `path` being `version`

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,10 @@ output "version" {
 
 output "location" {
   description = "The location of the artifact. Typically the S3 Bucket name or the ECR Registry URI."
-  value       = data.aws_ssm_parameter.version.value
+  value       = data.aws_ssm_parameter.location.value
 }
 
 output "path" {
   description = "Where the artifact is stored. Typically an S3 prefix or an ECR Repository name."
-  value       = data.aws_ssm_parameter.version.value
+  value       = data.aws_ssm_parameter.path.value
 }
